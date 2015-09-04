cc0683e4cc56c5fa3a594960363e9db0f9195848
JulianOrinyol <julianorinyol@gmail.com>
2015-09-04 00:49:40 -0400

    Refactored,  removed custom object attributeSeeker, and replaced it with hashes, as it is a simple enough data structure.

--- mbox_parser.rb
+++ mbox_parser.rb
@@ -5,56 +5,44 @@ require 'mbox'
   # attr_accessor :mime_version, :received, :date, :subject, :from, :to, :gmail_labels, :delivered_to, :message_id, :content_text, :content_html
 
 ### REGEX ###
-# labels = AttributeSeeker.new(?????, 'gmail_labels')
-MIME_version = AttributeSeeker.new(/MIME-Version:\s(.+)/, 'mime_version')
-received = AttributeSeeker.new(/Received:\sby\s([\d|\.]+).*;\s(.+)/, 'received')
-date = AttributeSeeker.new(/Date:\s(.+)/, 'date')
-# delivered_to = AttributeSeeker.new(?????, 'delivered_to')
-# message_id = AttributeSeeker.new(?????, 'message_id')
-subject = AttributeSeeker.new(/Subject:\s(.+)/, 'subject')
-from = AttributeSeeker.new(/From:\s(.+)\s<(.*@.*\..*)>/, 'from')
-to = AttributeSeeker.new(/To:\s(.+)\s<(.*@.*\..*)>/, 'to' )
-# message_plain = Attribute_seeker.new(?????, 'content_plain') # currently undefined
-# message_html = Attribute_seeker.new(?????, 'content_html') # currently undefined
+# labels = {regex: ?????, value: 'gmail_labels'}
+MIME_version = { regex: /MIME-Version:\s(.+)/, value: 'mime_version'}
+received = { regex: /Received:\sby\s([\d|\.]+).*;\s(.+)/, value: 'received'}
+date = {regex: /Date:\s(.+)/, value: 'date'}
+# delivered_to = {regex: ?????, value: 'delivered_to'}
+# message_id = {regex: ?????, value: 'message_id'}
+subject = {regex: /Subject:\s(.+)/, value: 'subject'}
+from = {regex: /From:\s(.+)\s<(.*@.*\..*)>/, value: 'from'}
+to = {regex: /To:\s(.+)\s<(.*@.*\..*)>/,  value:'to' }
+# message_plain = {regex: ?????, value: 'content_plain'} # currently undefined
+# message_html = {regex: ?????, value: 'content_html'} # currently undefined
 re_start = /X-GM-THRID:\s(.+)/ 
 re_boundary = /--(.+)--/ # Easy way to see where file ends, need to be smarter and use hash
 
-attribute_seekers = [MIME_version, received, date, subject, from, to] # add the others later... re_message_plain, re_message_html, etc.
+attributes = [MIME_version, received, date, subject, from, to] # add the others later... re_message_plain, re_message_html, etc.
 
-
-#  A simple object that simply associates a regex which returns a specific value, to its corresponding active record attribute name on the Message object.  Trying to think of name...  'AttributeSeeker' maybe... 
-class AttributeSeeker
-  attr_accessor :regex, :ar_attr
-
-  def initialize regex, ar_attr
-    @regex = regex
-    @ar_attr = ar_attr
-  end  
-end
 
 message = Message.create
 messages = []
-entire_message_string = []
-message_strings = []
 
 def parse_mbox_file file
   File.foreach(file) do |line|
-    attribute_seekers.each do |attribute_seeker|
-      value = attribute_seeker.regex.match(line) 
-      if value
-        message.update("#{attribute_seeker.ar_attr}": value)
+    if re_boundary.match(line)
+      messages << message
+      return
+    end
+
+    attributes.each do |attribute|
+      found_attribute = attribute[:regex].match(line) 
+      if found_attribute
+        message.update("#{attribute[:value]}": found_attribute) #do we need to call .strip anywhere here? on value? 
+
+        # remove the current attribute from the array so that other lines don't check for that attribute unnecessarily
+        attributes - [attribute]
+        break
       end
     end
 
-    entire_message_string << line.strip
-    if re_boundary.match(line)
-      puts "BEHOLD A MESSAGE"
-      message.save
-      message_strings << entire_message_string
-      messages << message
-      puts "END OF MESSAGE"
-      entire_message_string = []
-    end
   end
 end
 
